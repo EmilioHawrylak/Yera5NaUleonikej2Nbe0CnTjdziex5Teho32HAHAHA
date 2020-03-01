17c53bc8281cb4fe974ecc5c03ea5100c7c9c52a
Patrykosik88 <42325713+Patrykosik88@users.noreply.github.com>
2020-03-01 23:21:24 +0100

    Podnoszenie przedmiotów i zarys skryptu na ekwipunek

---
+++ gierka/STRAGANIKTHEGAME/Assets/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Equipment : MonoBehaviour
+{
+    static public GameObject[] Slots;
+    public int ile_obiektow;
+    void Start()
+    {
+        Slots = new GameObject[5];
+    }
+    static public void Equip(GameObject lol)
+    {
+        ; 
+    }
+}
