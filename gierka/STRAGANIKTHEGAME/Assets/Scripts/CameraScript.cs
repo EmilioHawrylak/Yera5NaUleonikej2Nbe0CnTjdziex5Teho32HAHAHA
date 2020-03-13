3e269ce288c02300eb6d923a3bf4ca2cea97be14
GIJ0P <GIJ0P@noreply.com>
2020-03-13 15:51:01 +0100

    Model jazdy
    Testowy samochód, działające zawieszenie, jazda przód/tył i skręcanie. Do poprawy parametry trakcji.

---
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform follow;
+    public Vector3 offset;
+    void Update()
+    {
+        transform.position = follow.position + offset;
+    }
+}
---
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/wheelModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wheelModel : MonoBehaviour
+{
+    public WheelCollider wheelC;
+    private Vector3 pos;
+    private Quaternion quat;
+    public Vector3 offset;
+    public Transform Car;
+    int orientation;
+
+    void Start()
+    {
+        orientation = 1;
+        if(wheelC.tag == "left")
+        {
+            orientation = -1;
+        }
+    }
+    void FixedUpdate()
+    {
+        wheelC.GetWorldPose(out pos, out quat);
+        transform.position = pos + offset;
+        transform.Rotate(0, 0, -wheelC.rpm / 60 * 360 * Time.deltaTime);
+        transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 90 * orientation + wheelC.steerAngle, transform.localEulerAngles.z);
+    }
+}
