464df1a1f1867af237a01bb3a05d12d02b4d1541
GIJ0P <GIJ0P@noreply.com>
2020-03-23 13:51:27 +0100

    coś

--- gierka/STRAGANIKTHEGAME/Assets/Scripts/CarScript.cs
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/CarScript.cs
@@ -10,7 +10,15 @@ public class CarScript : MonoBehaviour
     public WheelCollider RR;
     public float RPM = 10;
     public float steerSens;
+    public Transform camera;
+    public bool isInterior;
+    public Transform interiorCam;
+    public Transform chaseCam;
 
+    void Start()
+    {
+        isInterior = false;
+    }
     void Update()
     {
         RL.motorTorque = RPM * Input.GetAxisRaw("Vertical");
@@ -26,6 +34,18 @@ public class CarScript : MonoBehaviour
         }else{
             FL.steerAngle += Input.GetAxisRaw("Horizontal") * steerSens;
             FR.steerAngle += Input.GetAxisRaw("Horizontal") * steerSens;
+        }
+        if(isInterior)
+        {
+            camera.position = chaseCam.position;
+            camera.rotation = chaseCam.rotation;
+        } else{
+            camera.position = interiorCam.position;
+            camera.rotation = interiorCam.rotation;
+        }
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            isInterior = !isInterior;
         }
         Debug.Log(FR.steerAngle);
     }
