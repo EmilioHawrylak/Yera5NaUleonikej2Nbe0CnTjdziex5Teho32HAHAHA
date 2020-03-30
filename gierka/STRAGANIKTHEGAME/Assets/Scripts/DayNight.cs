d7777a7c7ee04c596946568a2daf56fd0d3a4bb9
Tomki2258 <52979692+Tomki2258@users.noreply.github.com>
2020-03-30 11:21:34 +0200

    Szczegóły o commicie w opisie
    Dodano:
    Scenę z logiem Studia + fade
    Dzień noc
    pause menu
    I coś jeszcze ale sory zapomniałem

---
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/DayNight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour
+{
+    public float speed = 1;
+    float mn;
+
+    
+    void Update()
+    {
+        mn = speed * Time.deltaTime;
+        transform.Rotate(Vector3.right*mn);
+    }
+}
