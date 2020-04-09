1afc40ab4f8a01a7358433314ff373d5027af888
Tomki2258 <52979692+Tomki2258@users.noreply.github.com>
2020-04-09 20:03:03 +0200

    Alkohol Start

---
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/alkohol.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alkohol : MonoBehaviour
+{
+    public bool inTrigger;
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Beer")
+        {
+            inTrigger = true;
+            Debug.Log("Pijesz");
+        }
+        if (collision.gameObject.name == "Wodka")
+        {
+            inTrigger = true;
+            Debug.Log("Pijesz");
+        }
+        if (collision.gameObject.name == "Whisky")
+        {
+            inTrigger = true;
+            Debug.Log("Pijesz");
+        }
+        if (collision.gameObject.name == "Spirytuss")
+        {
+            inTrigger = true;
+            Debug.Log("Pijesz");
+        }
+       
+    }
+    void OnTriggerExit(Collider collision)
+    {
+        inTrigger = false;
+    }
+    void OnGUI()
+    {
+        if (inTrigger)
+        {
+            GUI.Box(new Rect(700, 450, 200, 25), "Press E to drink");
+        }
+    }
+}
