433e52c7fb5998bb5434bd3616806f6e4c2fbef5
XCreckerX <33941168+XCreckerX@users.noreply.github.com>
2020-04-09 18:18:00 +0200

    Handel kontynuowany

---
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/TradingItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TradingItem : MonoBehaviour
+{
+    public Image ItemIcon;
+    public string ItemName;
+    public float Price;
+    public Text Description;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Description.text = ItemName + "\n" + Price.ToString() + "$";   
+    }
+}
