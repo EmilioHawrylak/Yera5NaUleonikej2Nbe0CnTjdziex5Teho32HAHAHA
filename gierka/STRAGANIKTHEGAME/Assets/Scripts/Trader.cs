8e562ce1f32f3d663854adba49b903f98372c0f0
XCreckerX <33941168+XCreckerX@users.noreply.github.com>
2020-08-05 20:00:26 +0200

    Sprzedawanie zaczęte

--- gierka/STRAGANIKTHEGAME/Assets/Scripts/Trader.cs
+++ gierka/STRAGANIKTHEGAME/Assets/Scripts/Trader.cs
@@ -7,15 +7,21 @@ public class Trader : MonoBehaviour
 {
     public PlayerStats Stats;
     public GameObject TraderPanel;
+    public GameObject SellPanel;
+    public GameObject BuyPanel;
     public PlayerMovement Player;
     public Text Money;
     public TradingItem Item;
     public bool canOpen;
     public NewEq Inventory;
+    public bool isBuying;
+    public bool isSelling;
 
     void Start()
     {
         TraderPanel.SetActive(false);
+        isBuying = true;
+        isSelling = false;
     }
 
     // Update is called once per frame
@@ -31,6 +37,19 @@ public class Trader : MonoBehaviour
             else if (!TraderPanel.activeInHierarchy)
             {
                 TraderPanel.SetActive(true);
+            }
+        }
+        if (TraderPanel.activeInHierarchy)
+        {
+            if (isSelling)
+            {
+                BuyPanel.SetActive(false);
+                SellPanel.SetActive(true);
+            }
+            if (isBuying)
+            {
+                BuyPanel.SetActive(true);
+                SellPanel.SetActive(false);
             }
         }
     }
@@ -49,5 +68,17 @@ public class Trader : MonoBehaviour
         {
             canOpen = false;
         }
+    }
+
+    public void TurnSellPanel()
+    {
+        isBuying = false;
+        isSelling = true;
+    }
+
+    public void TurnBuyPanel()
+    {
+        isSelling = false;
+        isBuying = true;
     }
 }
